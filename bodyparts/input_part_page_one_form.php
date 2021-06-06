638970262aee5341034627ea4a6108fbc1a57f46
Dmitriy <dizel007@yandex.ru>
2021-06-06 15:49:58 +0500

    Таблица работает. Нужно доработать оформление

--- bodyparts/input_part_page_one_form.php
+++ bodyparts/input_part_page_one_form.php
@@ -1,125 +1,231 @@
 <?php 
 
-$arr_for_upper_form = selectForCap($mysqli);
+if (!empty($_GET['maker']))       {  $maker = $_GET['maker'];  }
+if (!empty($_GET['typeProduct'])) {  $typeProduct = $_GET['typeProduct'];  }
+if (!empty($_GET['material']))    {  $material = $_GET['material'];  }
+if (!empty($_GET['dn']))          {  $dn = $_GET['dn'];  }
+if (!empty($_GET['width']))       {  $width = $_GET['width'];  }
+if (!empty($_GET['height']))      {  $height = $_GET['height'];  }
+if (!empty($_GET['load_class']))  {  $load_class  = $_GET['load_class'];  }
 
+
+$get_param=0; // количество передаваемых параметров в ГЕТ запросе
+!isset($maker)?$maker = "": $get_param++ ;
+!isset($typeProduct)?$typeProduct = "": $get_param++;
+!isset($material)?$material = "": $get_param++  ;
+!isset($dn)? $dn = "" : $get_param++ ;
+!isset($width)?$width = "": $get_param++  ;
+!isset($height)?  $height = "" : $get_param++ ;
+!isset($load_class)? $load_class = "": $get_param++ ;
+!isset($uklon)? $uklon = "": $get_param++ ;
+
+// echo "maker=". $maker."<br>";
+// echo "typeProduct=". $typeProduct."<br>";
+// echo "material=". $material."<br>";
+// echo "dn=". $dn."<br>";
+// echo "width=". $width."<br>";
+// echo "height=". $height."<br>";
+// echo "load_class=". $load_class."<br>";
+// echo "uklon=". $uklon."<br>";
+
+
+// Создаем массив по всем параметрам, есои не выбран ни один параметр, то выводим все таблицу
+$get_param>0 ? $arr_for_upper_form = selectForCapWhereAll($mysqli, $maker, $typeProduct, $material,$dn , $width , $height , $load_class):$arr_for_upper_form=selectAllArr($mysqli);
 $i = "";
-foreach ($arr_for_upper_form  as $arr) {
+foreach ($arr_for_upper_form as $arr) {
     $arr_maker[$i] = $arr['maker'];
+    $arr_typeProduct[$i] = $arr['typeProduct'];
+    $arr_material[$i] = $arr['material'];
+    $arr_dn[$i] = $arr['DN'];
     $arr_width[$i] = $arr['width'];
     $arr_height[$i] = $arr['height'];
     $arr_hload_class[$i] = $arr['load_class'];
  $i++;
  }
+
 $arr_maker = array_unique($arr_maker);
+$arr_typeProduct = array_unique($arr_typeProduct);
+$arr_material = array_unique($arr_material);
+$arr_dn = array_unique($arr_dn);
 $arr_width = array_unique($arr_width);
 $arr_height = array_unique($arr_height);
 $arr_hload_class = array_unique($arr_hload_class);
 
 asort($arr_maker);
+asort($arr_typeProduct);
+asort($arr_material);
+asort($arr_dn);
 asort($arr_width);
 asort($arr_height);
 asort($arr_hload_class);
 
 // var_dump($arr_new_width);
-echo "<form>
-    <div  class =\"up_form\">
+echo <<<HTML
+    
+  <form>
+    <div  class ="up_form">
+<!--     ТИП ПРОДУКТА   -->
+    <div class="mobile_web">
+    <select size="1" name="maker">
+HTML;
 
-    <div class=\"mobile_web\">
-    <select size=\"1\" name=\"maker\">
-        <option selected value=\"0\">Производитель</option>
-        <option value=\"1\">Gidrolica</option>
-        <option value=\"2\">StandartPark</option>
-        <option value=\"3\">MK ZHBI</option>
-        <option value=\"4\">Aquastok</option>
-    </select>
-    </div>
+switch ($maker) {
+    case 0: {$maker_temp = "Производитель"; break;}
+    case 1: {$maker_temp = "Gidrolica"; break;}
+    case 2: {$maker_temp = "StandartPark"; break;}
+    case 3: {$maker_temp = "MK ZHBI"; break;}
+    case 4: {$maker_temp = "Aquastok"; break;}
+    break;
+}
+echo  " <option selected value=".$maker.">".$maker_temp."</option>";
+  foreach ($arr_maker  as $arr_m) 
+    {
+        echo "<option maker=".$arr_m.">".$arr_m."</option>";
+    }
+   echo "</select> 
+         </div>";
+
+//    ТИП ПРОДУКТА   ********************************************************************        
+echo <<<HTML
+         <div class="mobile_web">
+         <select size="1" name="typeProduct">
+HTML;
 
 
-          <div class=\"mobile_web\">
-            <select size=\"1\" name=\"typeProduct\">
-                <option selected value=\"0\">Выберите тип товара</option>
-                <option value=\"1\">Лоток</option>
-                <option value=\"2\">Решетка</option>
-                <option value=\"3\">Комплект</option>
-                <option value=\"4\">Пескоуловитель</option>
-            </select>
-            </div>
+switch ($typeProduct) {
+    case 0: {$typeProduct_temp = "Тип продукции"; break;}
+    case 1: {$typeProduct_temp = "Лоток"; break;}
+    case 2: {$typeProduct_temp = "Пескоуловитель"; break;}
+    case 3: {$typeProduct_temp = "Решетка"; break;}
+    break;
+}
 
-            <div class=\"mobile_web\">
-                    <input type=\"checkbox\" name=\"uklon\" value=\"1\">Уклон
-            </div>
 
-            <div class=\"mobile_web\">
-              <select size=\"1\" name=\"material\">
-                  <option selected value=\"0\">Выберите материал</option>
-                  <option value=\"1\">Пластик</option>
-                  <option value=\"2\">Бетон</option>
-                  <option value=\"3\">Полимербетон</option>
-                  <option value=\"4\">Полимерпесок</option>
-              </select>
-            </div>
-            <div class=\"mobile_web\">
-            <select size=\"1\" name=\"dn\">
-                <option selected value=\"0\">Выберите DN</option>
-                <option value=\"9\">DN90</option>
-                <option value=\"1\">DN100</option>
-                <option value=\"11\">DN110</option>
-                <option value=\"15\">DN150</option>
-                <option value=\"16\">DN160</option>
-                <option value=\"2\">DN200</option>
-                <option value=\"3\">DN300</option>
-                <option value=\"4\">DN400</option>
-                <option value=\"5\">DN500</option>
-                
-            </select>
-            </div>
 
-            <div class=\"mobile_web\">
-                  <select size =\"1\" name=\"width\">
-                  <option selected value=\"0\">Выберите ширину</option>";
-                    foreach ($arr_width  as $arr_x) 
-                      {
-                          echo "<option width=\"".$arr_x."\">".$arr_x."</option>";
-                      }
-                      echo "</select>
-            </div>
-            
-            <div class=\"mobile_web\">
-            <select size =\"1\" name=\"height\">
-            <option selected value=\"0\">Выберите высоту</option>";
-              foreach ($arr_height  as $arr_x) 
-                {
-                    echo "<option height=\"".$arr_x."\">".$arr_x."</option>";
-                }
-                echo "</select>
-      </div>
+
+echo  " <option disabled >Тип продукции</option>";
+    echo "<option selected value=".$typeProduct.">".$typeProduct_temp."</option>";
+  foreach ($arr_typeProduct  as $arr_t) 
+    {
+        echo "<option maker=".$arr_t.">".$arr_t."</option>";
+    }
+    echo <<<HTML
+     </select>
      
-              <div class=\"mobile_web\">
-              <select size=\"1\" name=\"load_class\">
-                  <option selected value=\"0\">Выберите класс нагрузки</option>
-                  <option value=\"1\">A15</option>
-                  <option value=\"2\">B125</option>
-                  <option value=\"3\">C250</option>
-                  <option value=\"4\">D400</option>
-                  <option value=\"5\">E600</option>
-                  <option value=\"6\">F900</option>
-              </select>
-            </div>
+   </div>
+               <!-- ТИП ПРОДУКТА КОНЕЦ  ********************************************************************   -->
+<!--           UKLON          -->
+            <div class="mobile_web">
+                    <input type="checkbox" name="uklon" value="1">Уклон
+            </div> 
+HTML;
 
 
-            
-            <button type=\"submit\">ОБНОВИТЬ</button>
+
+//  ГИДРАВЛИЧЕСКОЕ СЕЧЕНГИЕ ****************************************************************
+
+
+echo <<<HTML
+            <div class="mobile_web">
+            <select size ="1" name="dn">
+HTML;
+echo  " <option disabled >Гидр.сечение</option>";
+echo  " <option selected value=\"".$dn."\">".$dn."</option>";
+                    foreach ($arr_dn as $arr_x) 
+                      {
+                          echo "<option dn=".$arr_x.">".$arr_x."</option>";
+                      }
+echo <<<HTML
+       </select>
+           </div>
+HTML;
+
+
+//  ТИП МАТЕРИАЛА ****************************************************************
+
+switch ($material) {
+    case 0: {$material_temp = "Материал"; break;}
+    case 1: {$material_temp = "Пластик"; break;}
+    case 2: {$material_temp = "Бетон"; break;}
+    case 3: {$material_temp = "Полимербетон"; break;}
+    case 4: {$material_temp = "Сталь"; break;}
+    case 5: {$material_temp = "чугун"; break;}
+    break;
+}
+
+echo <<<HTML
+            <div class="mobile_web">
+            <select size ="1" name="material">
+HTML;
+echo  " <option disabled >Материал</option>";
+echo  " <option selected value=\"".$material."\">".$material_temp."</option>";
+                    foreach ($arr_material as $arr_x) 
+                      {
+                          echo "<option material=".$arr_x.">".$arr_x."</option>";
+                      }
+echo <<<HTML
+       </select>
+           </div>
+HTML;
+
+
+
+//     ШИРИНА   ********************************************************************    
+echo <<<HTML
+         <div class="mobile_web">
+            <select size ="1" name="width">
+HTML;
+echo  " <option disabled >Ширина</option>";
+        echo  " <option selected value=\"".$width."\">".$width."</option>";
+                    foreach ($arr_width  as $arr_w) 
+                      {
+                          echo "<option width=".$arr_w.">".$arr_w."</option>";
+                      }
+echo <<<HTML
+       </select>
+     </div>
+HTML;
+
+//     ВЫСОТА     ********************************************************************    
+
+echo <<<HTML
+            <div class="mobile_web">
+            <select size ="1" name="height">
+HTML;
+echo  " <option disabled >Высота</option>";
+echo  " <option selected value=\"".$height."\">".$height."</option>";
+                    foreach ($arr_height as $arr_h) 
+                      {
+                          echo "<option height=".$arr_h.">".$arr_h."</option>";
+                      }
+echo <<<HTML
+       </select>
+           </div>
+HTML;
+
+
+
+
+
+echo <<<HTML
+
+             <button type="submit">ОБНОВИТЬ</button>
         </div>
     </div>
    
-</form>";
-
-
+</form>
+<form>
+<button type="submit">СБРОСИТЬ</button>
+        </div>
+    </div>
+   
+</form>
+HTML;
 
 
 // echo "
-// <form action=\"?.width=".$width."\" method=\"get\" id=\"foobar\">
-//     <select name=\"year\" onchange=\"document.getElementById('foobar').submit()\">
+// <form action="?.width=".$width."" method="get" id="foobar">
+//     <select name="year" onchange="document.getElementById('foobar').submit()">
 //         <option></option>
 //         <option>2010</option>
 //         <option>2011</option>
@@ -131,7 +237,7 @@ unset($arr_width);
 unset($arr_height);
 unset($arr_hload_class);
 
-unset($arr_for_upper_form);
+// unset($arr_for_upper_form);
 
 
 
--- functions/SelectFromDB.php
+++ functions/SelectFromDB.php
@@ -1,21 +1,82 @@
 <?php
 // $mysqli->query("SET NAMES 'utf8'");
-function selectForCap($mysqli) {
-  $sql = "SELECT DISTINCT  * FROM `line_drain_lotki`
-  LEFT JOIN `materials` ON line_drain_lotki.`material` = materials.`number`
-  LEFT JOIN `typeProduct` ON line_drain_lotki.`typeProduct` = typeProduct.`number`
-  ORDER BY line_drain_lotki.`id` ASC";
+function selectForCapWhereAll($mysqli, $maker, $typeProduct, $material, $dn , $width , $height , $load_class) {
+ $temp=0; 
+$col=0; // вводим переменную, чтобы понять сколько параметров выбрано
+if ($maker=="") {
+  
+        $sql_maker="";
+    } else {
+      $sql_maker="`maker`=$maker"; 
+      $col=1;
+    }
 
-$fQuery = $mysqli->query($sql);
+if ($col == 1) { // условие если нет первого параметра
+  empty($typeProduct)?$sql_typeProduct="":$sql_typeProduct=" AND `typeProduct`=$typeProduct";
+  } else {
+    empty($typeProduct)?$sql_typeProduct="":$sql_typeProduct=" `typeProduct`=$typeProduct";
+  }
+empty($typeProduct)?$temp=0:$col=1; // 
+
+if ($col == 1) { // условие если нет первого параметра
+    empty($material)?$sql_material="":$sql_material=" AND `material`=$material";
+    } else {
+      empty($material)?$sql_material="":$sql_material=" `material`=$material";
+    }
+ empty($material)?$temp=0:$col=1; // 
+
+ if ($col == 1) { // условие если нет первого параметра
+  empty($dn)?$sql_dn="":$sql_dn=" AND `dn`=$dn";
+  } else {
+    empty($dn)?$sql_dn="":$sql_dn=" `dn`=$dn";
+  }
+  empty($material)?$temp=0:$col=1; // 
+
+if ($col == 1) { // условие если нет первого параметра
+    empty($width)?$sql_width="":$sql_width=" AND `width`=$width";
+  } else {
+    empty($width)?$sql_width="":$sql_width=" `width`=$width";
+  }
+  empty($material)?$temp=0:$col=1; // 
+
+if ($col == 1) { // условие если нет первого параметра
+  empty($height)?$sql_height="":$sql_height=" AND `height`=$height";
+  } else {
+    empty($height)?$sql_height="":$sql_height=" `height`=$height";
+  }
+  empty($material)?$temp=0:$col=1; // 
+
+if ($col == 1) { // условие если нет первого параметра
+
+  empty($load_class)?$sql_load_class="":$sql_load_class=" AND `load_class`=$load_class";
+  } else {
+    empty($load_class)?$sql_load_class="":$sql_load_class=" `load_class`=$load_class";
+  }
+  empty($material)?$temp=0:$col=1; // 
+
+
+    $sql = "SELECT * FROM `line_drain_lotki` WHERE  $sql_maker $sql_typeProduct $sql_material $sql_dn $sql_width $sql_height $sql_load_class";
+echo "===".$sql."===<br>";
+  $fQuery = $mysqli->query($sql);
 //$arr_name = [];
+
 $arr_name = makeArrayFromObj($fQuery) ;
+
+// echo "<pre>";
+// var_dump ($arr_name);
+// echo "</pre>";
+
+
 return $arr_name;
 }
+
+
+
 // Из объекиа данных считанных из БД мы формируем для работы массив с этими данными (выводим весь объем данных)
 function selectAllArr($mysqli) {
            $sql = "SELECT DISTINCT  * FROM `line_drain_lotki`
-           LEFT JOIN `materials` ON line_drain_lotki.`material` = materials.`number`
-           LEFT JOIN `typeProduct` ON line_drain_lotki.`typeProduct` = typeProduct.`number`
+          --  LEFT JOIN `materials` ON line_drain_lotki.`material` = materials.`number`
+          --  LEFT JOIN `typeProduct` ON line_drain_lotki.`typeProduct` = typeProduct.`number`
            ORDER BY line_drain_lotki.`id` ASC";
 
 $fQuery = $mysqli->query($sql);
@@ -24,110 +85,4 @@ $fQuery = $mysqli->query($sql);
   return $arr_name;
 }
 
-////////////////////////// выводим таблицу с установленной шириной 
-
-function selectItemsByAllParam ($mysqli, $dn) {
-  $dn_real = "SELECT GidrSech FROM dn WHERE `dn`='$dn'";
-
-  if ($result = $mysqli->query($dn_real)) {
-    /* извлечение ассоциативного массива */
-    while ($row = $result->fetch_assoc()) {
-      $dnn = $row['GidrSech'];
-      // printf ($row['GidrSech']);
-    }
-}
-  $sql= "SELECT * FROM line_drain_lotki WHERE `dn`='$dnn'";
-  $fQuery = $mysqli->query($sql);
-  $arr_name = makeArrayFromObj($fQuery);
-return $arr_name;
-}
-
-
-
-
-
-
-function selectItemsByWidth ($mysqli, $width,$delta) {
-  $widthMin = $width - 5;
-  $widthMax = $width + 5;
-  $sql= "SELECT * FROM line_drain_lotki 
-  WHERE `width` >='$widthMin' AND
-  `width` <='$widthMax'";
-
-  $fQuery = $mysqli->query($sql);
-  $arr_name = makeArrayFromObj($fQuery);
-return $arr_name;
-
-}
-function selectItemsByHeight ($mysqli, $height) {
-  $sql= "SELECT * FROM line_drain_lotki WHERE `height`='$height'";
-  $fQuery = $mysqli->query($sql);
-  $arr_name = makeArrayFromObj($fQuery);
-return $arr_name;
-}
-
-
-
-function selectMakerFromBD ($mysqli, $maker) {
-  $maker_real = "SELECT maker FROM makers WHERE `number`='$maker'";
-
-  if ($result = $mysqli->query($maker_real)) {
-    /* извлечение ассоциативного массива */
-    while ($row = $result->fetch_assoc()) {
-      $maker = $row['maker'];
-      // printf ($row['GidrSech']);
-    }
-  }
-  return $maker;
-}
-function selectTypeProductFromBD ($mysqli, $typeProduct) {
-  $typeProduct_real = "SELECT typeProduct FROM typeproduct WHERE `number`='$typeProduct'";
-
-  if ($result = $mysqli->query($typeProduct_real)) {
-    /* извлечение ассоциативного массива */
-    while ($row = $result->fetch_assoc()) {
-      $typeProduct = $row['typeProduct'];
-        }
-  }
-  return $typeProduct;
-}
-
-
-function selectTypeMaterialFromBD ($mysqli, $material) {
-  $material_real = "SELECT material FROM materials WHERE `number`='$material'";
-
-  if ($result = $mysqli->query($material_real)) {
-    /* извлечение ассоциативного массива */
-    while ($row = $result->fetch_assoc()) {
-      $material = $row['material'];
-      // printf ($row['GidrSech']);
-    }
-  }
-  return $material;
-}
-function selectTypeDnFromBD ($mysqli, $dn) {
-   $dn_real = "SELECT GidrSech FROM gidro WHERE `number`='$dn'";
-
-  if ($result = $mysqli->query($dn_real)) {
-    /* извлечение ассоциативного массива */
-    while ($row = $result->fetch_assoc()) {
-      $dn = $row['GidrSech'];
-      // printf ($row['GidrSech']);
-    }
-  }
-  return $dn;
-}
-function selectTypeLoadClassFromBD ($mysqli, $load_class) {
-
-  $load_class_real = "SELECT load_class FROM load_classes WHERE `number`='$load_class'";
-
- if ($result = $mysqli->query($load_class_real)) {
-   /* извлечение ассоциативного массива */
-   while ($row = $result->fetch_assoc()) {
-     $load_class = $row['load_class'];
-     // printf ($row['GidrSech']);
-   }
- }
- return $load_class;
-}
-  ?>
+?>
